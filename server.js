f6bd8352b50333ac5c5ee652b7b84a596e69f64a
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-04-08 12:08:51 -0400

    commit to remove homepage route

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 // Route for homepage
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
 
 // Route for notes page
 app.get('/notes', (req, res) => {
