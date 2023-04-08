745685b498833f9b15402e5a75694a25351001b5
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-04-08 12:17:19 -0400

    commit to correct route paths in server.js

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(express.static('public'));
 
 // Route for notes page
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // Wildcare route to redirect to homepage
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
