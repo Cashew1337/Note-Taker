17cfc55f987dc1b337c9168d21a59fcb5c0a538f
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-04-08 12:00:06 -0400

    Commit of new routes, helpers.

---
+++ routes/notes.js
@@ -0,0 +1,55 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid')
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+
+notes.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) =>
+        res.json(JSON.parse(data))
+    );
+});
+
+notes.get('/:noteId', (req, res) => {
+    const noteId = req.params.noteId;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const results = json.filter((note) => note.noteId === noteId);
+            return results.length > 0
+            ? res.json(results)
+            : res.json('No note with requested ID found');
+        });
+});
+
+notes.post('/notes', (req, res) => {
+    console.log(req.body);
+
+    const { title, text, noteId } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            noteId: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added`);
+    } else {
+        res.errored('Issue adding note');
+    }
+});
+
+notes.delete('/:noteId', (req, res) => {
+    const noteId = req.params.noteId;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const results = json.filter((note) => note.noteId !== noteId);
+
+            writeToFile('./db/db.json', results);
+
+            res.json(`Note ${noteId} has been deleted`);
+        });
+});
+
+module.exports = notes
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+const api = require('./routes/index.js');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware used to parse JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+
+// Route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// Wildcare route to redirect to homepage
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:3001`)
+);
