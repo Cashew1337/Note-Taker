2b8f96b1d974c37df6ef7045bf73cdb65f5498fb
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-04-08 13:00:23 -0400

    add const path to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path')
 const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
